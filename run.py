dc9703ed049deb975bf6f87f3abce958e3205f84
gregcotech007 <commins.greg@outlook.com>
2022-02-20 21:38:24 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,46 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def new_game():
+    """
+    Initiate new quiz game
+    """
+    pass
+
+def player_fname():
+    """
+    Request player to enter their first name
+    """
+    pass
+
+def rules():
+    """
+    Provide user with instructions on how to play the game
+    """
+    pass
+
+def check_answer():
+    """
+    Check players answers, if they are correct, add 1 to score
+    """
+    pass
+
+def display_score():
+    """
+    Display the player's name and score results
+    """
+    pass
+
+def play_again():
+    """
+    Ask player if they wish to play new game, if not, print thank you and goodbye message
+    """
+    pass
+
+def welcome():
+    """
+    Quiz name (TBC) and Welcome Greeting
+    """
+    pass
+
+def main():
+    """
+    Run all program functions
+    """
